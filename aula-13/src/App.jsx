d05914797598b014d2acf96e3f9a570a8d3f2f70
Kayk magalhaes <kaykmagalhaesdiv@gmail.com>
2023-02-13 14:41:42 -0300

    Aula 13 e 14

---
+++ aula-13/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Contador from './components/contador'
+import Box from './components/Box'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Contador /> */}
+      <Box />
+    </div>
+  )
+}
+
+export default App
